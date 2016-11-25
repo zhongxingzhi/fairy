02c30b09c467c934663b1a1ea3f3324f2d2e432d
zhongxingzhi <zhongxingzhi2005@icloud.com>
2016-11-26 00:42:04 +0800

    add serialize support

--- serializer.py
+++ serializer.py
@@ -1,15 +1,22 @@
 import io
+import struct
 
+
+def is_event(obj):
+    return hasattr(obj, "__event_id__")
+
+def get_bytes(t, val):
+    return struct.pack("=i", val)
 
 class Serializer(object):
 
     def serialize(self, obj):
+        if not is_event(obj):
+            return None
+
         out_stream = io.BytesIO()
-        props = (getattr(obj, p) for p in dir(obj) if hasattr(getattr(obj, p), "__field_id__"))
-
-        #sort
-
-        for p in props:
-            p.serialize(out_stream)
+        fields = obj.__meta_fields__
+        for (fkey, fval) in fields:
+            out_stream.write(get_bytes(fval.__field_type__, getattr(obj, fkey)))
 
         return out_stream.getvalue()
