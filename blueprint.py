8f34579a5996b98796ae7ed5625cca94d1f129ea
zhongxingzhi <zhongxingzhi2005@icloud.com>
2016-11-26 09:21:02 +0800

    add packet id for inherit layer

--- blueprint.py
+++ blueprint.py
@@ -9,6 +9,13 @@ def echo_attributes(obj):
         print("{0}: {1}".format(a, getattr(obj, a)))
 
 
+def get_packet_id(cls, cls_name):
+    if cls.__name__ == cls_name:
+        return cls.__packet_id__
+    else:
+        return get_packet_id(cls.__bases__[0], cls_name)
+
+
 class FieldObject(object):
 
     def __init__(self, value_type):
@@ -21,8 +28,8 @@ class FieldObject(object):
         self.value = val
 
 def packet(cls):
-    if len(cls.__base__) == 1 and hasattr(cls.__base__[0], "__packet_id__"):
-        cls.__packet_id__ = cls.__base__[0].__packet_id__ + MAX_CLASS_FIELD_NUM
+    if len(cls.__bases__) == 1 and hasattr(cls.__bases__[0], "__packet_id__"):
+        cls.__packet_id__ = cls.__bases__[0].__packet_id__ + MAX_CLASS_FIELD_NUM
     else:
         cls.__packet_id__ = 0
 
@@ -37,7 +44,10 @@ def event(event_id):
         for attr in dir(cls):
             fi = getattr(cls, attr)
             if hasattr(fi, "__field_id__"):
-                echo_attributes(fi)
+                #echo_attributes(fi)
+                cls_name = fi.__qualname__.split('.')[0]
+                packet_id = get_packet_id(cls, cls_name)
+                print("packet_id = {0}, cls_name = {1}".format(packet_id, cls_name))
                 cls.__meta_fields__[attr] = (fi.__field_id__, fi.__field_type__)
                 setattr(cls, attr, FieldObject(fi.__field_type__))
 
